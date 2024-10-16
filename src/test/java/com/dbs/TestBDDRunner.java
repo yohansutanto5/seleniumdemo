cb0018ae8c3a1062aff994ddb1512fb96016bf41
yohan <yohan.sutanto@outlook.com>
2024-10-17 06:45:21 +0700

    rename

--- src/test/java/com/dbs/TestBDDRunner.java
+++ src/test/java/com/dbs/TestBDDRunner.java
@@ -1,6 +1,7 @@
 package  com.dbs;
 
 import org.junit.runner.RunWith;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
@@ -10,5 +11,5 @@ import io.cucumber.junit.CucumberOptions;
     glue = "com.dbs.pages",                 // Path to step definitions
     plugin = {"pretty", "html:target/cucumber-reports.html"}  // Reporting options
 )
-public class TestRunner {
+public class TestBDDRunner {
 }
